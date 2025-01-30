1bad4698cffa48111033b03615b4cf68f2821493
zoya <gergenkopzoya@gmail.com>
2025-01-30 22:37:00 +0700

    sem1

---
+++ app/Commands/HandleEventsCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Commands;
+
+use App\Application;
+
+use App\Database\SQLite;
+
+use App\EventSender\EventSender;
+
+use App\Models\Event;
+
+//use App\Models\EventDto;
+
+class HandleEventsCommand extends Command
+
+{
+
+    protected Application $app;
+
+    public function __construct(Application $app)
+
+    {
+
+        $this->app = $app;
+
+    }
+
+    public function run(array $options = []): void
+
+    {
+
+        $event = new Event(new SQLite($this->app));
+
+        $events = $event->select();
+
+        $eventSender = new EventSender();
+        foreach ($events as $event) {
+	    echo $this->shouldEventBeRan($event);
+            if ($this->shouldEventBeRan($event)) {
+                $eventSender->sendMessage($event["receiver_id"], $event["text"]);
+            }
+
+        }
+
+    }
+
+    private function shouldEventBeRan($event): bool
+{
+    $currentMinute = date("i");
+    $currentHour = date("H");
+    $currentDay = date("d");
+    $currentMonth = date("m");
+    $currentWeekday = date("w");
+
+    return (
+        ($event['minute'] === null || $event['minute'] == $currentMinute) &&
+        ($event['hour'] === null || $event['hour'] == $currentHour) &&
+        ($event['day'] === null || $event['day'] == $currentDay) &&
+        ($event['month'] === null || $event['month'] == $currentMonth) &&
+        ($event['day_of_week'] === null || $event['day_of_week'] == $currentWeekday)
+    );
+}
+
+
+}
